bb4746b650b1e2a1a3589e8b59ba5a204feb605e
kolavn <veda.kola@gmail.com>
2026-01-04 16:52:12 -0500

    Palindromic Pattern 1234

---
+++ Palindromic_Pattern.py
@@ -0,0 +1,14 @@
+def palindromic_pyramid(n: int) -> None:
+    for i in range(1, n + 1):
+        # print leading spaces
+        print(" " * (n - i), end="")
+
+        # increasing numbers
+        for j in range(1, i + 1):
+            print(j, end="")
+
+        # decreasing numbers
+        for j in range(i - 1, 0, -1):
+            print(j, end="")
+
+        print()
